f174c144a80ab01f5611eb9acfa0ca8c9a541060
Igor Costa <igorcosta@github.com>
2024-09-24 16:14:59 +1200

    adding releaser

--- main.go
+++ main.go
@@ -8,10 +8,19 @@ import (
 	"github.com/igorcosta/gh-lazy/pkg/utils"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+)
+
 func main() {
 	if err := cmd.Execute(); err != nil {
 		utils.LogError(err, "Command execution failed")
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
+	}
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Printf("Version: %s, Commit: %s\n", version, commit)
+		return
 	}
 }
