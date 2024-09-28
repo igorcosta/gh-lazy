223253ce7feb1f0f150b66528e1779113bc59fb5
Igor Costa <igorcosta@github.com>
2024-09-28 17:49:09 +1200

    versioning

--- main.go
+++ main.go
@@ -6,21 +6,22 @@ import (
 
 	"github.com/igorcosta/gh-lazy/cmd"
 	"github.com/igorcosta/gh-lazy/pkg/utils"
-)
-
-var (
-	version = "dev"
-	commit  = "none"
+	"github.com/igorcosta/gh-lazy/pkg/version"
 )
 
 func main() {
+	// Handle version flag
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Println(version.GetVersionInfo())
+		return
+	}
+
+	// Print version information at startup
+	fmt.Printf("gh-lazy %s\n", version.Version)
+
 	if err := cmd.Execute(); err != nil {
 		utils.LogError(err, "Command execution failed")
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
-	}
-	if len(os.Args) > 1 && os.Args[1] == "--version" {
-		fmt.Printf("Version: %s, Commit: %s\n", version, commit)
-		return
 	}
 }
