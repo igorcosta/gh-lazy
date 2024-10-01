d6e9814ade284d2ba0ae72e860cf8bdd1de4463f
Igor Costa <igorcosta@github.com>
2024-10-01 23:36:41 +1300

    lets try semver

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,18 +5,14 @@ import (
 	"runtime"
 )
 
-// Version is the current version of the extension.
-// This field is automatically updated by the CI/CD pipeline.
-const Version = "v0.1.0"
-
-// BuildDate is the date when the binary was built.
-var BuildDate = "unknown"
-
-// GitCommit is the git commit hash of the build.
-var GitCommit = "unknown"
+var (
+	Version   = "dev"
+	Commit    = "none"
+	BuildDate = "unknown"
+)
 
 // GetVersionInfo returns a formatted string with full version information.
 func GetVersionInfo() string {
 	return fmt.Sprintf("Version: %s\nGit Commit: %s\nBuild Date: %s\nGo Version: %s\nOS/Arch: %s/%s",
-		Version, GitCommit, BuildDate, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+		Version, Commit, BuildDate, runtime.Version(), runtime.GOOS, runtime.GOARCH)
 }
